bd385d51d7d2c524b8138926c7c968926b6a7630
Jordan Giacone <Jmgiacone@gmail.com>
2014-10-25 15:34:48 -0500

    Added units to insertion operator of Item

--- Item.h
+++ Item.h
@@ -11,7 +11,7 @@ struct Item
 
 std::ostream& operator << (std::ostream& out, Item i)
 {
-  out << "[" << i.name << ": Value " << i.value << ", Weight " << i.weight << ", Ratio " << i.ratio << "]";
+  out << "[" << i.name << ": Value $" << i.value << ", Weight " << i.weight << "lb, Ratio " << i.ratio << " $/lb]";
   return out;
 }
 #endif // ITEM_H
