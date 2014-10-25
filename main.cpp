718836b3921a0296a0cae1cabfc8b2ceeb3eb086
Jordan Giacone <Jmgiacone@gmail.com>
2014-10-25 15:35:22 -0500

    Made a simple program that generates strictly positive floats and assigns them to the variables in Item.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,49 @@
+#include <random>
+#include <ctime>
 #include "Item.h"
-
 using namespace std;
 
 int main()
 {
-  Item i;
-  i.name = "Excalibur";
-  i.value = 100000;
-  i.weight = 1;
-  i.ratio = i.value / i.weight;
+    int n;
+    float mean, stdev, temp;
+    cout << "How many elements in the array? ";
+    cin >> n;
+    cout << "What should the mean of the values be? ";
+    cin >> mean;
+    cout << "What should the standard deviation of the values be? ";
+    cin >> stdev;
 
-  cout << i << endl;
-  return 0;
+    Item* wares = new Item[n];
+    float* values = new float[2*n];
+    normal_distribution<float> rng(mean, stdev);
+    default_random_engine randEngine(time(NULL));
+
+    for(int i = 0; i < 2*n; i++)
+    {
+        do
+        {
+            temp = rng(randEngine);
+        }
+        while(temp <= 0);
+
+        values[i] = temp;
+    }
+
+    int j = 0;
+    for(int i = 0; i < n; i++)
+    {
+        wares[i].name = "Item";
+        wares[i].value = values[j];
+        wares[i].weight = values[j + 1];
+        wares[i].ratio = wares[i].value / wares[i].weight;
+        j+= 2;
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << wares[i] << endl;
+    }
+    
+    return 0;
 }
