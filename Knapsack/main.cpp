dd7a7c18a59a63c745da2051f93daa24399ea2d0
Jordan Giacone <Jmgiacone@gmail.com>
2014-10-24 23:15:17 -0500

    Very basic main function that tests the Item struct

--- Knapsack/main.cpp
+++ Knapsack/main.cpp
@@ -1,1 +1,15 @@
+#include "Item.h"
 
+using namespace std;
+
+int main()
+{
+  Item i;
+  i.name = "Excalibur";
+  i.value = 100000;
+  i.weight = 1;
+  i.ratio = i.value / i.weight;
+
+  cout << i << endl;
+  return 0;
+}
