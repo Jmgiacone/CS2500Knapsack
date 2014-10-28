297486206f020c19c5c1682ca0952f96db60dbc0
Jordan Giacone <Jmgiacone@gmail.com>
2014-10-28 11:19:35 -0500

    Added in a loop that will run the function and then average the times

--- main.cpp
+++ main.cpp
@@ -9,9 +9,14 @@ int bruteForceKnapsack(Item* items, int n, int w);
 
 int main()
 {
-    int n, w, temp;
-    float mean, variance, duration;
+    int n, w, temp, numRepetitions;
+    float mean, variance, duration, totalDuration = 0;
     clock_t start, end;
+    Item* wares = NULL;
+    int* values = NULL;
+    default_random_engine randEngine(time(NULL));
+
+
     cout << "What is the capacity of the knapsack? ";
     cin >> w;
     cout << "How many elements in the array? ";
@@ -20,36 +25,54 @@ int main()
     cin >> mean;
     cout << "What should the variance of the values be? ";
     cin >> variance;
-    Item* wares = new Item[n];
-    int* values = new int[2 * n];
+    cout << "How many repetitions should be performed? ";
+    cin >> numRepetitions;
+
     normal_distribution<float> rng(mean, sqrt(variance));
-    default_random_engine randEngine(time(NULL));
-
-    for(int i = 0; i < 2 * n; i++)
+    for(int x = 0; x < numRepetitions; x++)
     {
-        do
+        wares = new Item[n];
+        values = new int[2 * n];
+
+        for (int i = 0; i < 2 * n; i++)
         {
-            temp = static_cast<int>(rng(randEngine));
+            do
+            {
+                temp = static_cast<int>(rng(randEngine));
+            }
+            while (temp <= 0);
+
+            values[i] = temp;
         }
-        while(temp <= 0);
 
-        values[i] = temp;
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            wares[i].value = values[j];
+            wares[i].weight = values[j + 1];
+            wares[i].ratio = wares[i].value / static_cast<float>(wares[i].weight);
+            j += 2;
+        }
+
+        start = clock();
+        for(int i = 0; i < n; i++)
+        {
+            cout << wares[i] << endl;
+        }
+        cout << "The answer via the dynamic method is: " << dynamicKnapsack(wares, n, w) << endl;
+        end = clock();
+        duration = (end - start) / static_cast<float>(CLOCKS_PER_SEC);
+        cout << "This operation took " << duration << "ms" << endl;
+        totalDuration += duration;
+
+        delete [] wares;
+        delete [] values;
+
+        wares = NULL;
+        values = NULL;
     }
 
-    int j = 0;
-    for(int i = 0; i < n; i++)
-    {
-        wares[i].value = values[j];
-        wares[i].weight = values[j + 1];
-        wares[i].ratio = wares[i].value / static_cast<float>(wares[i].weight);
-        j+= 2;
-    }
-
-    start = clock();
-    cout << "The answer via the dynamic method is: " << dynamicKnapsack(wares, n, w) << endl;
-    end = clock();
-    duration = (end - start) / static_cast<float>(CLOCKS_PER_SEC);
-    cout << "This operation took " << duration << "ms" << endl;
+    cout << "The average execution time is: " << (duration / numRepetitions) << "ms" << endl;
     return 0;
 }
 
